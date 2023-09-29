4e9c4f8a4fd63cb56fa691b97ca08b3ef1129404
osja21ab <91535591+osja21ab@users.noreply.github.com>
2023-09-29 12:32:05 +0200

    camera + QR function

--- Views/LocationDetailScreen.js
+++ Views/LocationDetailScreen.js
@@ -1,48 +1,100 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
-import productsData from '../products.json'; // Importér produktdatalisten fra JSON-filen
-//randomkommentar
+import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+import { Camera } from 'expo-camera';
+import productsData from '../products.json';
+
 const LocationDetail = () => {
   const [products, setProducts] = useState([]);
+  const [isCameraVisible, setCameraVisible] = useState(false);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scannedData, setScannedData] = useState(null);
 
   useEffect(() => {
-    // Læs JSON-data, når komponenten indlæses
     fetchProducts();
+    askCameraPermission();
   }, []);
 
   const fetchProducts = () => {
-   
-    setProducts(productsData); // Indlæs produktdatalisten fra JSON-filen 
+    setProducts(productsData);
   };
 
-  // Funktion til at bestemme billedkilden baseret på billedstien i produktdatalisten
   const getImageSource = (imagePath) => {
     switch (imagePath) {
       case './paddle.png':
-        return require('./paddle.png'); 
-      
+        return require('./paddle.png');
       default:
         return null;
     }
   };
 
+  const askCameraPermission = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    setHasPermission(status === 'granted');
+  };
+
+  const handleProductPress = () => {
+    setCameraVisible(true);
+  };
+
+  const handleBarcodeRead = (event) => {
+    const scannedData = event.data;
+    console.log('Scanned Data:', scannedData);
+
+    // Optionally, perform additional actions with the scanned data here
+    // setScannedData(scannedData);
+    setCameraVisible(false); // Close the camera after scanning
+
+    // Open the scanned URL
+    if (scannedData.startsWith('http://') || scannedData.startsWith('https://')) {
+      Linking.openURL(scannedData);
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Produktliste</Text>
-      <FlatList
-        data={products} // Brug produktdatalisten fra det lokale miljø
-        keyExtractor={(item) => item.id.toString()} // Konverter id til en streng for nøgle
-        renderItem={({ item }) => (
-          <View style={styles.productItem}>
-            <Image
-              source={getImageSource(item.image)} // Hent billedkilden baseret på stien i JSON filen
-              style={styles.productImage}
-            />
-            <Text>{item.name}</Text>
-            <Text>{item.description}</Text>
-          </View>
-        )}
-      />
+      {/* Camera Component */}
+      {isCameraVisible && (
+        <Camera
+          style={styles.fullScreenCamera}
+          type={Camera.Constants.Type.back}
+          onBarCodeScanned={handleBarcodeRead}
+        />
+      )}
+
+      {!isCameraVisible && (
+        <>
+          <Text style={styles.title}>Produktliste</Text>
+          <FlatList
+            data={products}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={handleProductPress}>
+                <View style={styles.productItem}>
+                  <Image
+                    source={getImageSource(item.image)}
+                    style={styles.productImage}
+                  />
+                  <Text>{item.name}</Text>
+                  <Text>{item.description}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
+        </>
+      )}
+
+      {scannedData && (
+        <View style={styles.scannedDataContainer}>
+          <Text style={styles.scannedDataText}>Scanned Data: {scannedData}</Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -53,6 +105,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  fullScreenCamera: {
+    ...StyleSheet.absoluteFillObject,
   },
   title: {
     fontSize: 24,
@@ -71,6 +126,15 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     resizeMode: 'contain',
+  },
+  scannedDataContainer: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#e0e0e0',
+    borderRadius: 5,
+  },
+  scannedDataText: {
+    fontSize: 16,
   },
 });
 
