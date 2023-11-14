02f4156547fe475b4aa47980118680c8e3be80d0
madspedersen35 <mads.pedersen35@hotmail.com>
2023-11-14 12:55:32 +0100

    payment methods done
    done

--- components/PaymentScreen.js
+++ components/PaymentScreen.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, Alert, StyleSheet, ScrollView, Image, Modal } from 'react-native';
-import { getFirestore, collection, doc, addDoc, getDocs } from 'firebase/firestore';
+import { View,Text,TextInput,Button, Alert,StyleSheet,ScrollView,Image,Modal,TouchableOpacity,} from 'react-native';
+import { getFirestore, collection, doc, addDoc, getDocs, deleteDoc } from 'firebase/firestore';
 import { app, auth } from '../firebase/fireBase';
 import { Feather } from '@expo/vector-icons';
 
-
-
-const PaymentScreen = ({navigation}) => {
-    
+const PaymentScreen = ({ navigation }) => {
   const [cardNumber, setCardNumber] = useState('');
   const [expiry, setExpiry] = useState('');
   const [cvv, setCVV] = useState('');
@@ -75,61 +72,142 @@ const PaymentScreen = ({navigation}) => {
     }
   };
 
+  const deleteCard = async (cardId) => {
+    try {
+      const db = getFirestore(app);
+      const user = auth.currentUser;
+
+      if (user) {
+        const userEmail = user.email.toLowerCase();
+        const userDocRef = doc(db, 'Users', userEmail);
+        const paymentMethodDocRef = doc(userDocRef, 'PaymentMethod', cardId);
+
+        await deleteDoc(paymentMethodDocRef);
+
+        Alert.alert('Card Deleted', 'Card has been successfully deleted.');
+        getCards();
+      } else {
+        console.log('No user logged in.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: 'Add Payment Method',
+      headerStyle: {
+        backgroundColor: '#095167',
+      },
+      headerTintColor: '#FCCE85',
+      headerTitleStyle: {
+        fontSize: 18,
+      },
+      headerLeft: () => (
+        <Feather
+          name="arrow-left"
+          size={24}
+          color="#FCCE85"
+          onPress={() => navigation.navigate('ProfileScreen')}
+          style={{ marginLeft: 10 }}
+        />
+      ),
+    });
+  }, [navigation]);
+
+  const handleCardNumberChange = (input) => {
+    // Remove any non-numeric characters
+    const numericInput = input.replace(/[^\d]/g, '');
+
+    // Insert a space every 4 digits
+    const formattedInput = numericInput.replace(/(\d{4})/g, '$1 ').trim();
+
+    // Limit total length to 19 characters
+    const limitedInput = formattedInput.slice(0, 19);
+
+    setCardNumber(limitedInput);
+  };
+
+  const handleExpiryChange = (input) => {
+    // Remove any non-numeric characters
+    const numericInput = input.replace(/[^\d]/g, '');
+
+    // Format the input as MM/YY
+    const formattedInput = numericInput.replace(/(\d{0,2})(\d{0,2})/, (_, p1, p2) => (p1 ? p1 + (p2 ? '/' + p2 : '') : ''));
+
+    setExpiry(formattedInput);
+  };
+
+  const handleCVVChange = (input) => {
+    // Remove any non-numeric characters
+    const numericInput = input.replace(/[^\d]/g, '');
+
+    // Limit to a maximum of 3 digits
+    const limitedInput = numericInput.slice(0, 3);
+
+    setCVV(limitedInput);
+  };
+
   const selectCardType = (type) => {
     setCardType(type);
     setShowCardTypePicker(false);
   };
 
-  useEffect(() => {
-    navigation.setOptions({
-        title: 'Add Payment Method',
-        headerStyle: {
-          backgroundColor: '#095167',
-        },
-        headerTintColor: '#FCCE85',
-        headerTitleStyle: {
-          fontSize: 18, // Change the title font size
-        },
-        headerLeft: () => (
-          <Feather // Use Feather icon for the back button
-            name="arrow-left"
-            size={24}
-            color="#FCCE85"
-            onPress={() => navigation.navigate('ProfileScreen')}
-            style={{ marginLeft: 10 }}
-          />
-        ),
-      });
-    }, [navigation]);
   return (
     <View style={styles.container}>
       <Text style={styles.sectionTitle}>Add Payment Method</Text>
-      <TextInput
-        style={styles.input}
-        value={cardNumber}
-        onChangeText={setCardNumber}
-        keyboardType="numeric"
-        placeholder="Enter card number"
-      />
-      <View style={{ flexDirection: 'row' }}>
-  <TextInput
-    style={styles.inputdate}
-    value={expiry}
-    onChangeText={setExpiry}
-    keyboardType="numeric"
-    placeholder="MM/YY"
-  />
-  <TextInput
-    style={styles.inputcvv}
-    value={cvv}
-    onChangeText={setCVV}
-    keyboardType="numeric"
-    placeholder="CVV"
-  />
-</View>
 
-      <View style={styles.cardTypeContainer}>
-        <Button title={`Card Type: ${cardType || 'Select'}`} onPress={() => setShowCardTypePicker(!showCardTypePicker)} />
+      <View style={styles.cardInputContainer}>
+        <View style={styles.inputRow}>
+          <View style={styles.cardIcon}>
+            <Feather name="credit-card" size={20} color="#095167" />
+          </View>
+          <TextInput
+            style={styles.input}
+            value={cardNumber}
+            onChangeText={handleCardNumberChange}
+            keyboardType="numeric"
+            placeholder="Enter card number"
+            placeholderTextColor="#b3b3b3"
+            maxLength={19}
+          />
+        </View>
+
+        <View style={styles.inputRow}>
+          <View style={styles.cardIcon}>
+            <Feather name="calendar" size={20} color="#095167" />
+          </View>
+          <TextInput
+            style={styles.inputdate}
+            value={expiry}
+            onChangeText={handleExpiryChange}
+            keyboardType="numeric"
+            maxLength={5}
+            placeholder="MM/YY"
+            placeholderTextColor="#b3b3b3"
+          />
+
+          <View style={styles.cardIcon}>
+            <Feather name="lock" size={20} color="#095167" />
+          </View>
+          <TextInput
+            style={styles.inputcvv}
+            value={cvv}
+            onChangeText={handleCVVChange}
+            keyboardType="numeric"
+            placeholder="CVV"
+            placeholderTextColor="#b3b3b3"
+/>
+        </View>
+      </View>
+
+      <TouchableOpacity
+          style={styles.cardTypeButton}
+          onPress={() => setShowCardTypePicker(!showCardTypePicker)}
+        >
+          <Text style={styles.cardTypeButtonText}>{`Card Type: ${cardType || 'Select'}`}</Text>
+        </TouchableOpacity>
         <Modal
           animationType="slide"
           transparent={true}
@@ -137,47 +215,60 @@ const PaymentScreen = ({navigation}) => {
           onRequestClose={() => setShowCardTypePicker(false)}
         >
           <View style={styles.modalView}>
-            <Button title="Visa" color={'#FCCE85'} onPress={() => selectCardType('Visa')} />
-            <Button title="Mastercard" color={'#FCCE85'} onPress={() => selectCardType('Mastercard')} />
-            <Button title="Dankort" color={'#FCCE85'} onPress={() => selectCardType('Dankort')} />
+            <TouchableOpacity style={styles.cardTypePickerOption} onPress={() => selectCardType('Visa')}>
+              <Text style={styles.cardTypePickerOptionText}>Visa</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.cardTypePickerOption} onPress={() => selectCardType('Mastercard')}>
+              <Text style={styles.cardTypePickerOptionText}>Mastercard</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.cardTypePickerOption} onPress={() => selectCardType('Dankort')}>
+              <Text style={styles.cardTypePickerOptionText}>Dankort</Text>
+            </TouchableOpacity>
           </View>
         </Modal>
-      </View>
 
-      <Button title="Add Card" onPress={addCardToFirestore} />
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={addCardToFirestore}
+      >
+        <Text style={styles.addButtonLabel}>Add Card</Text>
+      </TouchableOpacity>
 
       <Text style={styles.sectionTitle}>Your Cards</Text>
       <ScrollView>
-        {cards.length === 0 ? (
-          <Text style={styles.noCardText}>You have no cards added.</Text>
-        ) : (
-          cards.map((card) => (
-            <View key={card.id} style={styles.cardContainer}>
-              <View style={styles.cardDetails}>
-                <Text style={styles.cardDetailstext}>Card Number: {card.cardNumber}</Text>
-                <Text style={styles.cardDetailstext}>Expiry: {card.expiry}</Text>
-                <Text style={styles.cardDetailstext}>CVV: {card.cvv}</Text>
-                <Text style={styles.cardDetailstext}>Card Type: {card.cardType}</Text>
-              </View>
-              <Image
-                source={
-                  card.cardType === 'Visa'
-                    ? require('../assets/visa.png')
-                    : card.cardType === 'Mastercard'
-                    ? require('../assets/mastercard.png')
-                    : card.cardType === 'Dankort'
-                    ? require('../assets/dankort.png')
-                    : null
-                }
-                style={styles.cardImage}
-              />
-            </View>
-          ))
-        )}
-      </ScrollView>
-    </View>
+  {cards.length === 0 ? (
+    <Text style={styles.noCardText}>You have no payment method, please add one above!</Text>
+  ) : (
+    cards.map((card) => (
+      <View key={card.id} style={styles.cardContainer}>
+        <View style={styles.cardDetails}>
+          <Text style={styles.cardDetailstext}>Card Number: **** **** **** {card.cardNumber.slice(-4)}</Text>
+          <Text style={styles.cardDetailstext}>Expiry: {card.expiry}</Text>
+          <Text style={styles.cardDetailstext}>CVV: ***</Text>
+        </View>
+        <Image
+          source={
+            card.cardType === 'Visa'
+              ? require('../assets/visa.png')
+              : card.cardType === 'Mastercard'
+              ? require('../assets/mastercard.png')
+              : card.cardType === 'Dankort'
+              ? require('../assets/dankort.png')
+              : null
+          }
+          style={styles.cardImage}
+        />
+        <TouchableOpacity onPress={() => deleteCard(card.id)}>
+          <Feather name="trash-2" size={24} color="#d11a2a" style={{ marginLeft: 10 }} />
+        </TouchableOpacity>
+      </View>
+    ))
+  )}
+</ScrollView>
+</View>
   );
 };
+
 
 const styles = StyleSheet.create({
   container: {
@@ -193,61 +284,73 @@ const styles = StyleSheet.create({
     color: '#095167',
     alignSelf: 'center',
   },
+  inputContainer: {
+    marginBottom: 10,
+  },
   input: {
     height: 45,
     borderColor: '#095167',
     borderWidth: 1,
-    marginBottom: 10,
     paddingLeft: 10,
     borderRadius: 20,
-    width: 350,
+    width: 300,
     alignSelf: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 10,
   },
   inputdate: {
     height: 45,
     borderColor: '#095167',
     borderWidth: 1,
-    marginBottom: 10,
     paddingLeft: 10,
     borderRadius: 20,
-    width: 140,
+    width: 130,
     marginRight: 10,
-    marginLeft: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   inputcvv: {
     height: 45,
     borderColor: '#095167',
     borderWidth: 1,
-    marginBottom: 10,
     paddingLeft: 10,
     borderRadius: 20,
-    width: 140,
-    
-  
-
+    width: 130,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   noCardText: {
     fontSize: 16,
     color: 'gray',
   },
   cardContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     padding: 20,
     backgroundColor: '#095167',
     borderRadius: 20,
     marginBottom: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-  
-
+    elevation: 3, // Add elevation for a slight shadow on Android
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
   },
   cardDetails: {
     flex: 1,
-  
+    marginRight: 20,
   },
   cardDetailstext: {
     color: '#FCCE85',
-    fontSize: 12,
     fontWeight: 'bold',
+    fontSize: 12,
     marginBottom: 5,
   },
   cardImage: {
@@ -259,24 +362,64 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 10,
-     
+  },
+  cardInputContainer: {
+    marginBottom: 10,
+  },
+  cardIcon: {
+    marginRight: 7,
+  },
+  inputRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  addButton: {
+    backgroundColor: '#4CAF50', // Set your desired green color
+    borderRadius: 20,
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  addButtonLabel: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  cardTypeButton: {
+    backgroundColor: '#FCCE85', // Set your desired color
+    borderRadius: 20,
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  
+  cardTypeButtonText: {
+    color: '#095167', // Set your desired text color
+    fontWeight: 'bold',
+    fontSize: 16,
   },
   modalView: {
-    margin: 50,
-    backgroundColor: '#095167',
-    marginTop: '90%',
-    borderRadius: 20,
-    padding: 10,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 10,
-      height: 10,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
-});
+  
+  cardTypePickerOption: {
+    backgroundColor: '#FCCE85',
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#095167',
+    alignItems: 'center',
+  },
+  
+  cardTypePickerOptionText: {
+    color: '#095167',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});  
 
 export default PaymentScreen;
